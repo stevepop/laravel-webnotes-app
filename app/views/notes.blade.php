774cd843bdffb5d5a77e5f63ea7f02276c50dd5f
Steve Popoola <stevepop@gmail.com>
2014-03-22 13:23:09 +0000

    Wrapped div around each stickynote to ensure only parent of delete button is removed on delete

--- app/views/notes.blade.php
+++ app/views/notes.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     <div id="sticky-notes">
         @foreach ($notes as $note)
-        <textarea id="stickynote- {{ $note->id }}"> {{ $note->note }}</textarea>
-        <a href="#" id="remove-{{ $note->id }}" class="delete-sticky">X</a>
+         	<div class="sticky-note">
+        		<textarea id="stickynote- {{ $note->id }}"> {{ $note->note }}</textarea>
+        		<a href="#" id="remove-{{ $note->id }}" class="delete-sticky">X</a>
+        	</div>
         @endforeach
         <div id="create">+</div>
     </div>
